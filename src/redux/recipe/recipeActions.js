368b3e86d3a6c624d78f8a875f2abcdc2079f7e8
Lukas Jan <lukasjanonis@gmail.com>
2019-09-20 13:45:55 +0300

    Updated env settings

--- src/redux/recipe/recipeActions.js
+++ src/redux/recipe/recipeActions.js
@@ -12,22 +12,11 @@ import {
   SET_SEARCH_FOCUS
 } from './recipeTypes';
 
-let recipeApiKey;
-
-if (process.env.NODE_ENV !== 'production') {
-  recipeApiKey = process.env.REACT_APP_RECIPE_API_KEY;
-  console.log(recipeApiKey);
-} else {
-  //test
-  recipeApiKey = process.env.RECIPE_API_KEY;
-  console.log(recipeApiKey);
-}
-
 // Search API for recipes
 export const searchRecipes = (searchQuery, history) => async dispatch => {
   try {
     const res = await axios.get(
-      `https://www.food2fork.com/api/search?key=${recipeApiKey}&q=${searchQuery}`
+      `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_RECIPE_API_KEY}&q=${searchQuery}`
     );
     if (res.data.error) {
       const err = `API ${res.data.error} reached, please try again later`;
@@ -49,7 +38,7 @@ export const searchRecipes = (searchQuery, history) => async dispatch => {
 export const getCurrentRecipe = recipeID => async dispatch => {
   try {
     const res = await axios.get(
-      `https://www.food2fork.com/api/get?key=${recipeApiKey}&rId=${recipeID}`
+      `https://www.food2fork.com/api/get?key=${process.env.REACT_APP_RECIPE_API_KEY}&rId=${recipeID}`
     );
     const data = res.data.recipe;
     dispatch({
